e8022a0321448adc1b8dc42eb6ae2419c76bbfd7
kimzzyguru <valentinekimani13@gmail.com>
2026-02-11 09:17:19 +0300

    first commit

---
+++ python-basics/challenge1.py
@@ -0,0 +1,9 @@
+University="JKUAT"
+Grade="A"
+Favourite_course="economics and finance"
+cluster_points= 48
+print(f"My favourite University is {University}")
+print(f"My grade is {Grade}")
+print(f"my favourite course is {Favourite_course}")
+print(f"my cluster points are {cluster_points}")
+print(f"my full profile is: University: {University}, Grade: {Grade}, Favourite Course: {Favourite_course}, Cluster Points: {cluster_points}")
---
+++ python-basics/hello.py
@@ -0,0 +1,14 @@
+# This is a comment
+print("Hello from Valentine")
+# City/town
+print("I from Thika")
+# my hobbies
+print("I like to watching movies, listening to music,cooking,playing tennis")
+
+age=25
+height=1.75
+weight=75
+
+print(f"I am {age} years old") 
+print(f"I am also {height} metres")
+print(f"And weigh {weight} kgs")
