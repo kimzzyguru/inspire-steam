696dd33bb52cd36a774fbfad2f8bd74d8a829165
kimzzyguru <valentinekimani13@gmail.com>
2026-02-17 06:02:00 +0300

    assignment

---
+++ python-basics/assignment3.py
@@ -0,0 +1,20 @@
+#Name: Valentine Kimani
+#Date: 13/02/2026
+#Program to calculate geometric progression
+
+# Calculating nth term of a geometric progression
+
+import math
+
+for y in range (-180,+180,30):
+      print(f"sine of {y}={math.sin (y)}")
+for y in range (-180,+180,30):
+      print(f"cosine of {y}={math.cos (y)}")
+for y in range (-180,+180,30):
+      print(f"tangent of {y}={math.tan (y)}")
+print("----------------------------------")
+print("Angle(y)   |   Sine(y)        |  Cosine(y)               |   Tangent(y)")
+for y in range (-180,+180,30):
+      print(f"|{y}|  {math.sin (y)}|     {math.cos (y)}|     {math.tan (y)}")
+     
+        
--- python-basics/forloop.py
+++ python-basics/forloop.py
@@ -12,14 +12,3 @@ for x in range (0,360,30):
     print(math.tan (x))
 for i in range (10,0,-1):
     print(i)
-for y in range (-180,+180,30):
-      print(f"sine of {y}={math.sin (y)}")
-for y in range (-180,+180,30):
-      print(f"cosine of {y}={math.cos (y)}")
-for y in range (-180,+180,30):
-     print("-------------------------------")
-     print(f"angle={y}")
-     print(f"sine of {y}={math.sin (y)}")
-     print(f"cosine of {y}={math.cos (y)}")
-        
-     print(f"tangent of {y}={math.tan (y)}")
