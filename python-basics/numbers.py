9f254e0425333f7fdc13be88972fa5dfcb4d7141
kimzzyguru <valentinekimani13@gmail.com>
2026-02-19 21:04:13 +0300

    Assignment 7

--- python-basics/numbers.py
+++ python-basics/numbers.py
@@ -10,7 +10,7 @@ sum_numbers=first_number+second_number
 #product_numbers=first_number*second_number
 #quotient_numbers=first_number/second_number
 
-print(f"The sum of the numbers is: {sum_numbers}")
+#print(f"The sum of the numbers is: {sum_numbers}")
 #print(f"The difference of the numbers is: {difference_numbers}")
 #print(f"The product of the numbers is: {product_numbers}")
 #print(f"The quotient of the numbers is: {quotient_numbers}")
